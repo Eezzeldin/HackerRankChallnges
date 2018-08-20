b80b8b4a79b966098dafd46da5dd477ce3b2ffe4
Emohamed <emad.ezzeldin4@gmail.com>
2018-08-20 11:40:02 -0400

    Problem Formulation

--- sliding_window.py
+++ sliding_window.py
@@ -6,28 +6,32 @@ Challenge : Birthday Chocolate
 
 ##Variables
 Input_Sample = [1,2,1,3,2]     # 5 bars of chocolate with 1 written on them
-m            = 2               # Window Size
+m            = 3               # Window Size
 d            = 3               # Desired Total
 s            = 1               # Slide_Size
 m0           = 0               # Window Start index
 w            = []              # Window
 counter      = 0
+iterations   = len (Input_Sample) - (m -1) #Number of iterations
+print ("iterations:" , iterations)
+
+##Initiatlization
 #Trial 1 :
-#if Input_Sample [0] + Input_Sample [0+1] == 3 : counter+=1
-#print (counter)
 w = [Input_Sample [i] for i in range (m0,m0+m)]
-print (w)
+print ("Window:",w)
 if sum (w) == d : counter+=1
-print (counter)
+print ("counter:",counter)
+
+##Iterations 
 #Trial 2 :
 m0+=s
 w = [Input_Sample [i] for i in range (m0,m0+m)]
-print (w)
+print ("Window:",w)
 if sum (w) == d : counter+=1
-print (counter)
-#Trial 3
+print ("counter:",counter)
+#Trial 3 :
 m0+=s
 w = [Input_Sample [i] for i in range (m0,m0+m)]
-print (w)
+print ("Window:",w)
 if sum (w) == d : counter+=1
-print (counter)
+print ("counter:",counter)
