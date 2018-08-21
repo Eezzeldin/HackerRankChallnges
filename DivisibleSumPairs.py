aa1851b9e4cb5f146bdaff03ed543f83aee39246
Emohamed <emad.ezzeldin4@gmail.com>
2018-08-21 11:03:55 -0400

    Final
    Window Slides
    Inner Window Slides
    Logic is Checked

--- DivisibleSumPairs.py
+++ DivisibleSumPairs.py
@@ -6,10 +6,6 @@ Challenge: Divisible Sum Pairs
 
 ## Variables
 input_Sample = [1, 3, 2, 6, 1, 2]
-# long_hit     = [[i,j] for j in input_Sample[1:] for i in input_Sample[:input_Sample.index(j)] if (i+j) % 3 is 0]
-# print (long_hit)
-# combinations     = [[i,input_Sample[j]] for j in range(0,5,-1) for i in range (0,j-1)]
-# print (combinations)
 l            = len (input_Sample)
 pairs        = []
 i            = 0
@@ -20,19 +16,17 @@ j1           =  1    # sliding starting index
 j2           =  l    # sliding ending index
 k            = 3
 
-
 ## Operations
-#  Logic
 def DivisbleSum (ii,ij):
     m            =  ii  + ij  #math
     b            = m % k == 0 # condition
     return b
 
-#  Window Length Forward Expansion
-i = i1                   # Fixing i at start point
-ii=  input_Sample [i]    #current value of i
-for j in range (i+1,j2): #Slide j
-    ij           =  input_Sample [j] #current value of j
-    print (ii , ij)
-    if DivisbleSum (ii,ij): pairs.append ([ii,ij]) # Run Logic
-print (pairs)            #Print output
+## Slide Variable Sized Window
+for i in range (i1,i2):
+    ii=  input_Sample [i]                              #current value of i
+    for j in range (i+1,j2):                           #Slide j
+        ij           =  input_Sample [j]               #current value of j
+        print (ii , ij)
+        if DivisbleSum (ii,ij): pairs.append ([ii,ij]) # Run Logic
+    print (pairs)                                      #Print output
