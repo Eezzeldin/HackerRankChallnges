4907906cf6d647dcca2635895aa9a74bc826af99
Emohamed <emad.ezzeldin4@gmail.com>
2018-08-22 10:02:07 -0400

    Correct Answer but Too Slow
    I have just seen one of their test cases, and the list was like 12,000
    elements at least in it. So I need to find a faster way.

---
+++ MigratoryBird.py
@@ -0,0 +1,57 @@
+'''
+Author : Emad Ezzeldin
+Date   : Tuesday August 21st 2018
+Challenge : MigratoryBird
+'''
+
+input_Sample = [1, 4, 4, 4, 5, 3,3,8,8,8]
+l            = len (input_Sample)
+pairs        = []
+repeated     = {}
+for element in input_Sample: repeated [element] = 0
+i            = 0
+j            = 0
+i1           = 0     # fixed starting index
+i2           = l - 1 # end stop index
+j1           =  1    # sliding starting index
+j2           =  l    # sliding ending index
+k            = 3
+
+## Operations
+def DivisbleSum (ii,ij):
+    m            =  ii  + ij  #math
+    b            = ii == ij # condition
+    return b
+
+## Slide Variable Sized Window
+for i in range (i1,i2):
+    ii=  input_Sample [i]                              #current value of i
+    for j in range (i+1,j2):                           #Slide j
+        ij           =  input_Sample [j]               #current value of j
+        print (ii)
+        if DivisbleSum (ii,ij):
+            pairs.append ([ii,ij]) # Run Logic
+            repeated[ii] = repeated[ii] + 1
+    print (repeated)                                      #Print output
+
+#print (max(repeated))
+
+## Max Count Operations
+#Get List of Counts
+Counts = [element for element in repeated.values()]
+print (Counts)
+#Find Max Count
+MaxC  = max (Counts)
+print (MaxC)
+## Logic
+#Find Elements with Max Count : Find Keys where values are MaxC
+MaxCE = [element for element in repeated if repeated [element] is MaxC]
+print (MaxCE)
+#Check if they are more than one
+Final = []
+if len (MaxCE) >1 :
+    Final =  min (MaxCE)
+#If More than one return their min.
+else:
+    Final = MaxCE[0]
+print (Final)
