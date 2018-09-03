af492981230f5c0d28d34f975486166ff72bcd9c
Emohamed <emad.ezzeldin4@gmail.com>
2018-09-03 10:17:59 -0400

    Poly Regression is in Place

---
+++ PolynomialRegression_OfficePrices/Modeling/Build/Modeling_Build.py
@@ -0,0 +1,62 @@
+import numpy as np
+import pandas as pd
+
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import Ridge
+from sklearn.preprocessing import PolynomialFeatures
+np.set_printoptions(precision=2)
+
+# synthetic dataset for more complex regression
+from sklearn.datasets import make_friedman1
+X_F1, y_F1 = make_friedman1(n_samples = 100,
+                           n_features = 7, random_state=0)
+
+X_train, X_test, y_train, y_test = train_test_split(X_F1, y_F1,
+                                                   random_state = 0)
+linreg = LinearRegression().fit(X_train, y_train)
+
+print('linear model coeff (w): {}'
+     .format(linreg.coef_))
+print('linear model intercept (b): {:.3f}'
+     .format(linreg.intercept_))
+print('R-squared score (training): {:.3f}'
+     .format(linreg.score(X_train, y_train)))
+print('R-squared score (test): {:.3f}'
+     .format(linreg.score(X_test, y_test)))
+
+print('\nNow we transform the original input data to add\n\
+polynomial features up to degree 2 (quadratic)\n')
+poly = PolynomialFeatures(degree=2)
+X_F1_poly = poly.fit_transform(X_F1)
+
+X_train, X_test, y_train, y_test = train_test_split(X_F1_poly, y_F1,
+                                                   random_state = 0)
+linreg = LinearRegression().fit(X_train, y_train)
+
+print('(poly deg 2) linear model coeff (w):\n{}'
+     .format(linreg.coef_))
+print('(poly deg 2) linear model intercept (b): {:.3f}'
+     .format(linreg.intercept_))
+print('(poly deg 2) R-squared score (training): {:.3f}'
+     .format(linreg.score(X_train, y_train)))
+print('(poly deg 2) R-squared score (test): {:.3f}\n'
+     .format(linreg.score(X_test, y_test)))
+
+print('\nAddition of many polynomial features often leads to\n\
+overfitting, so we often use polynomial features in combination\n\
+with regression that has a regularization penalty, like ridge\n\
+regression.\n')
+
+X_train, X_test, y_train, y_test = train_test_split(X_F1_poly, y_F1,
+                                                   random_state = 0)
+linreg = Ridge().fit(X_train, y_train)
+
+print('(poly deg 2 + ridge) linear model coeff (w):\n{}'
+     .format(linreg.coef_))
+print('(poly deg 2 + ridge) linear model intercept (b): {:.3f}'
+     .format(linreg.intercept_))
+print('(poly deg 2 + ridge) R-squared score (training): {:.3f}'
+     .format(linreg.score(X_train, y_train)))
+print('(poly deg 2 + ridge) R-squared score (test): {:.3f}'
+     .format(linreg.score(X_test, y_test)))
