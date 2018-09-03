35f43bd148a9a96f8cea054384f3bcb9546c2ed2
Emohamed <emad.ezzeldin4@gmail.com>
2018-09-03 18:10:18 -0400

    Modeling Finished
    I successfully used the Template of the linear regression on coursera
    course on the data i was given and I successfully outputted the
    prediction of the model on hacker rank test data.

--- PolynomialRegression_OfficePrices/Modeling/Build/Modeling_Build.py
+++ PolynomialRegression_OfficePrices/Modeling/Build/Modeling_Build.py
@@ -8,6 +8,11 @@ from sklearn.datasets import make_friedman1
 X_F1, y_F1 = make_friedman1(n_samples = 100,
                            n_features = 7, random_state=0)
 
+print ("=="*40)
+print ("complex reg")
+print (X_F1)
+
+
 DataPath = '/Users/emadezzeldin/Dropbox/PhD/HackerRank/HackerRankChallnges/PolynomialRegression_OfficePrices/Modeling/Data/'
 X_train  =  pd.read_csv (DataPath+'TrainingData_Predictors.csv')
 Y_train  =  pd.read_csv (DataPath+ 'TrainingData_Responce.csv')
@@ -15,11 +20,13 @@ Y_train  = Y_train ['Responce']
 print ('=='*40)
 print ('Training Data Predictors')
 print (X_train)
+print ('Training Data Predictors')
+print (X_train.values)
 print ('=='*40)
 print ('Training Data Responce')
 print (Y_train)
 
-X_F1, y_F1 = X_train , Y_train
+X_F1, y_F1 = X_train.values , Y_train.values
 
 from sklearn.linear_model import LinearRegression
 from sklearn.linear_model import Ridge
@@ -27,6 +34,12 @@ from sklearn.preprocessing import PolynomialFeatures
 
 X_train, X_test, y_train, y_test = train_test_split(X_F1, y_F1,
                                                    random_state = 0)
+
+print ("X_Test")
+print (X_test)
+print ("y_Test")
+print (y_test)
+
 linreg = LinearRegression().fit(X_train, y_train)
 
 print('linear model coeff (w): {}'
@@ -73,3 +86,37 @@ print('(poly deg 2 + ridge) R-squared score (training): {:.3f}'
      .format(linreg.score(X_train, y_train)))
 print('(poly deg 2 + ridge) R-squared score (test): {:.3f}'
      .format(linreg.score(X_test, y_test)))
+
+print ("=="*40)
+print("Test Data")
+print(X_test)
+
+x_test   =  pd.read_csv (DataPath+'TestData_Predictors.csv')
+print ("=="*40)
+print ("Real Test Data")
+print (x_test.values)
+
+Test_F1_poly = poly.fit_transform(x_test.values)
+print (Test_F1_poly)
+
+#
+# #np.reshape(np.array(DF['Brain']),(-1,1))
+# X_test   = x_test.values
+# print ("=="*40)
+# print ("Formatted Test Data")
+# print (X_test)
+
+
+# Sample = np.array(x_test)
+# print ('Sample')
+# print (Sample)
+#
+# Reshaped = np.reshape (Sample,(1,-1))
+# print ("Reshaped")
+# print (Reshaped)
+
+
+Predictions = linreg.predict(Test_F1_poly)
+print ("=="*40)
+print ("Predictions")
+print (Predictions)
