5868e19e1d55f290a4b9f81e4fe77245fafe2c86
Emohamed <emad.ezzeldin4@gmail.com>
2018-10-13 13:15:08 -0400

    ReadingData Dev

--- StockPrediction/Dev/readingData_Dev.py
+++ StockPrediction/Dev/readingData_Dev.py
@@ -5,21 +5,12 @@ import os
 DataFile        = os.path.join (os.getcwd(),'../DataFiles/train.txt')
 Data_Handle     = open (DataFile)
 Data_text       = Data_Handle.read()
-print ("=="*40)
-print (Data_text)
-print ("=="*40)
+
 
 StocksData_text       = Data_text.split('\n')[:-1]
-print ("=="*40)
-print (StocksData_text[0])
-print (StocksData_text[5])
-print (StocksData_text[-1])
-print ("=="*40)
-
 StockNames       = [Stock.split() [0] for Stock in StocksData_text]
-print (StockNames)
 StockData        = [Stock.split() [1:] for Stock in StocksData_text]
-print (StockData)
 
 StockData_DF    =  pd.DataFrame (index = StockNames, data = StockData)
-print (StockData_DF)
+DataOut         =  os.path.join (os.getcwd(),'../DataFiles/StockData.csv')
+StockData_DF.to_csv (DataOut,index = True,header=False)
