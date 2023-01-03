bcbf764b1db27cf9d43836756c576860956b1be9
Emad Mohamed <emad.ezzeldin4@gmail.com>
2023-01-03 16:29:12 -0500

    Version_1_CeasarCipher
    The first attempt at solving the CeasarCipher problem. It passed 7/12 tests and failed at this one
    
    10
    www.abc.xy
    87
    
    fff.jkl.gh

---
+++ Ceasar_Cipher/Ceasar_Cipher_test.py
@@ -0,0 +1,60 @@
+'''
+Original alphabet:      abcdefghijklmnopqrstuvwxyz
+Alphabet rotated +3:    defghijklmnopqrstuvwxyzabc
+'''
+
+cipher = {}
+n = 3
+
+#abcdefghijklmnopqrstuvwxyz
+letters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q",
+"R","S","T","U","V","W","X","Y","Z"]
+#print (len (letters))
+
+letters_2 = [letters [letters.index(i) + n] for i in letters [:-n]]
+#print (letters_2)
+
+letters_3 = [letters [i] for i in range (len (letters [-n:]))]
+#print (letters_3)
+
+letters_4 = letters_2 + letters_3
+#print (letters_4)
+
+for i,j in zip (letters, letters_4): cipher [i] = j
+#print (cipher)
+
+
+# encoded = ""
+# word    = "abcdefghij-klmnopq-rstuvwxyz"
+# for w in word :
+#     if w.upper() in letters:
+#         print (cipher [w.upper()])
+#
+#         if w.islower ():
+#             encoded = encoded + cipher [w.upper()].lower()
+#         else:
+#             encoded = encoded + cipher [w.upper()]
+#     else:
+#         encoded = encoded + w
+# print (encoded)
+
+
+def cipher_word (word):
+    encoded = ""
+    #word    = "abcdefghij-klmnopq-rstuvwxyz"
+    for w in word :
+        if w.upper() in letters:
+            #print (cipher [w.upper()])
+
+            if w.islower ():
+                encoded = encoded + cipher [w.upper()].lower()
+            else:
+                encoded = encoded + cipher [w.upper()]
+        else:
+            encoded = encoded + w
+    return encoded
+
+test_word_1 = "abcdefghij-klmnopq-rstuvwxyz"
+test_word_2 = "There's-a-starman-in-the-sky"
+print (cipher_word (test_word_1))
+print (cipher_word (test_word_2))
